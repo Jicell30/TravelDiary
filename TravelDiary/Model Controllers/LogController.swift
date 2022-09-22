96cec375574f0a8726d6e7a97841494eb8326e3b
Jicell <jicellcollado@Jicells-MacBook-Air.local>
2022-09-22 11:37:51 -0400

    Model controller almost complete

---
+++ TravelDiary/Model Controllers/LogController.swift
@@ -0,0 +1,38 @@
+//
+//  LogController.swift
+//  TravelDiary
+//
+//  Created by Jicell on 9/22/22.
+//
+
+import Foundation
+
+class LogController {
+    
+    //Sigleton:To ensure that we are with the same instance of our logs array we created a singleton and will pass around this specific instance of our log controller
+    
+    //MARK: -Singleton
+    static let sharedInstance = LogController()
+    
+    //MARK: - Source of Thruth ( single )
+    var logs: [Log] = []
+    
+    // MARK: -CRUD
+    
+    func create(title: String, address: String, body: String) {
+        let log = Log(title: title, address: address, body: body)
+        logs.append(log)
+    }
+    
+    func update(logToUpdate: Log, newAddress: String, newBody: String, newTitle: String) {
+        logToUpdate.title = newTitle
+        logToUpdate.address = newAddress
+        logToUpdate.body = newBody
+        logToUpdate.logDate = Date()
+    }
+    
+    func delete(logToDelete:Log) {
+        
+    }
+    
+} //End of class
